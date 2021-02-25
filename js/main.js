c218605851319568d15e95cdcc30a3e101914b69
Chafik El idrissi <c.elidrissi@thirard.fr>
2021-02-25 17:59:51 +0100

    Big commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 var products = []; // Tableau des produits
 var online; // Variable de connexion // Permet de bloquer des actions en mode 'Hors Ligne'
+var preview_height; // Variable contenant la largeur de la preview // #PasOuf #Triche
+var preview_width; // Variable contenant la largeur de la preview // #PasOuf #Triche
+var assetsMainNames = ['img-1.jpg','img-2.jpg','img-3.jpg','img-4.jpg']; // Nom des assets pour l'affichage principal
 
 // Fonction lancé lorsque le chargement de la page est terminé
 window.onload = function(){
+    preview_height = document.querySelector('.grid_preview_main').clientHeight;
+    preview_width = document.querySelector('.grid_preview_main').clientWidth;
     // En fonction des données en sessionStroage, on affiche ou non la banniere
     if (sessionStorage.getItem('banner') === 'false') {
         document.querySelector('.banner').style.display = 'none';
@@ -11,13 +16,19 @@ window.onload = function(){
     }
 }
 
+// Fonction lancé au redimensiionnement de la page
+window.onresize = function(){
+    alert("Il s'emblerait que votre page est été redimensionné, rechargement ..");
+    location.reload();
+}
+
 // Appel de la fonction stickyMenu au scroll
 window.onscroll = function() {stickyMenu()};
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function stickyMenu() {
     // Foncton qui ajoute la classe homemade-sticky
-    var menu = document.getElementsByClassName("menu")[0];
+    var menu = document.getElementsByClassName("grid_header")[0];
     var sticky = menu.offsetTop;
     // On active la classe seulement quand le scroll arrive à la possition (Y) du menu
     if (window.pageYOffset > sticky) {
@@ -48,32 +59,44 @@ function setUpCarrousel(){
     for(id_product in products){
         var p = products[id_product];
         var el = document.createElement('div');
-        el.className = (id_product >= 4 && 'carrousel-hide') || 'carrousel-column'; // On n'affiche que les 4 premiers éléments
-        el.innerHTML = '<img class="carrousel-thumnbail carrousel-cursor" src="' + p.imageUrl + '" style="width:100%" onclick="showProduct('+ p.id + ')" alt="'+ p.title +'">'
+        el.className = (id_product >= 6 && 'carrousel-hide') || 'carrousel-column'; // On n'affiche que les 4 premiers éléments
+        el.innerHTML = '<img class="carrousel-thumnbail carrousel-cursor" src="' + p.imageUrl + '" onclick="showProduct('+ p.id + ')" alt="'+ p.title +'">'
         document.querySelector('.grid_preview_second').append(el);
     }
 }
 
-function setUp(data){
-    setUpProducts(data);
-    setUpCarrousel();
-    showProduct(1); 
-}
-
 // Affichage du produit séléctionné
 function showProduct(id){
-    console.log('ShowProduct('+id-1+')');
-
-    // Photo Principale
-    var p = products[id-1];
-    var el = document.createElement('div');
-    el.innerHTML = '<img src="' + p.imageUrl + '" style="width:100%" alt="'+ p.title +'">'
-    document.querySelector('.grid_preview_main').innerHTML = el.innerHTML;
-
-    // Description
-    var el = document.createElement('div');
-    el.innerHTML = '<'
+    var name = assetsMainNames[id];
+    document.querySelector('.grid_preview_main').firstChild.src = './assets/' + name;
 }
+
+// SetUp des zonnes d'affichage
+function setUpMain(){
+    for(i=3; i>=0; i--){
+        var name = assetsMainNames[i]
+        var el = document.createElement('div')
+        if (name == 'img-4.jpg'){
+            // Affichage de la premiere photo dans zone principal
+            el.innerHTML = '<img style="max-width:100%; max-height:100%" src="./assets/' + name + '" onclick="showProduct('+ i + ')"' + name + '">'
+            document.querySelector('.grid_preview_main').innerHTML = el.innerHTML;
+        }
+        // Affichage des photos dans la zone secondaire
+        el.className = 'carrousel-thumnbail carrousel-cursor';
+        var w = document.querySelector('.grid_preview_second').clientWidth/4 - 10;
+        el.innerHTML = '<img class="carrousel-thumnbail carrousel-cursor" style="width:' + w + 'px" src="./assets/' + name + '"onclick="showProduct('+ i + ')" alt="' + name + '">'
+        document.querySelector('.grid_preview_second').innerHTML += el.innerHTML;
+    }
+}
+
+function setUp(data){
+    setUpMain()
+    //setUpProducts(data);
+    //setUpCarrousel();
+    //showProduct(1); 
+}
+
+
 
 // Api Axios qui récupére les données
 function callApi(){
